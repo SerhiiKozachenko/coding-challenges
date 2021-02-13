9372ee5037503cfc609ea12dc4f0343bf4e3e670
Sergey Kozachenko <sergiy.kozachenko@gmail.com>
2021-02-13 22:58:21 +0200

    leet-code-2021 cpp xcode

---
+++ leet-code-2021/leet-code-2021/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leet-code-2021
+//
+//  Created by Sergey Kozachenko on 13.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
